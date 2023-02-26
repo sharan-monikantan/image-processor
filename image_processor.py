eaef198d38e35966bb32a9a07ede25177708e574
Sharan Kumar <2835079+sharan-monikantan@users.noreply.github.com>
2023-02-26 14:39:44 +0530

    Mosaic imagery

--- image_processor.py
+++ image_processor.py
@@ -1,8 +1,12 @@
+import glob
 import logging
 from pathlib import Path
+import shutil
 import zipfile
 
 import geopandas
+import rasterio as rio
+from rasterio.merge import merge
 from sentinelsat import geojson_to_wkt, read_geojson, SentinelAPI
 
 
@@ -44,8 +48,44 @@ def extract_imagery():
             zipped_file.extractall('./sentinel_imagery')
 
 
+def collect_band_for_resolution(resolution, band):
+    logging.info('Collecting band ' + band)
+    Path('./sentinel_imagery/'+band).mkdir(parents=True, exist_ok=True)
+    data = glob.glob('./sentinel_imagery/**/'+resolution+'/*'+band+'.jp2', recursive=True)
+    for _ in data:
+        shutil.copy(_, './sentinel_imagery/'+band)
+
+
+def mosaic_imagery(band):
+    """
+    Reference: URL
+    """
+    files_for_band = list(Path('./sentinel_imagery/'+band).iterdir())
+    files_to_mosaic = []
+    for _ in files_for_band:
+        raster = rio.open(_)
+        files_to_mosaic.append(raster)
+
+    mosaic, output = merge(files_to_mosaic)
+    output_metadata = raster.meta.copy()
+    output_metadata.update({
+        "driver": "GTiff",
+        "height": mosaic.shape[1],
+        "width": mosaic.shape[2],
+        "transform": output
+    })
+    with rio.open('./sentinel_imagery/'+band+'/mosaic.tiff', "w", **output_metadata) as _:
+        print(output_metadata)
+        _.write(mosaic)
+
+
 if __name__ == '__main__':
     download_sentinel_imagery()
 
     # Pre-process imagery
     extract_imagery()
+    for resolution, band in [('R10m', 'B04_10m'), ('R10m', 'B08_10m'), ('R20m', 'B04_20m'), ('R20m', 'B8A_20m')]:
+        collect_band_for_resolution(resolution, band)
+    for band in ['B04_10m', 'B08_10m', 'B04_20m', 'B8A_20m']:
+        logging.info('Mosaic-ing band ' + band)
+        mosaic_imagery(band)
